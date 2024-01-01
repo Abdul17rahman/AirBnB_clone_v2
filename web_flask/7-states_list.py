07bb1b68ae30900e748cad7b8c843a042fdaa20d
Abdul Rahman <abdulnsamba@gmail.com>
2024-01-02 00:22:03 +0300

    sort-1

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -14,14 +14,15 @@ app = Flask(__name__)
 @app.route('/states_list', strict_slashes=False)
 def state_list():
     """ Displays a list of states"""
-    states = sorted(list(storage.all(State).values()), key=lambda x: x.name)
+    states = sorted(list(storage.all(State).values()), key=lambda state: state.name)
     return render_template('7-states_list.html', states=states)
 
 
 @app.teardown_appcontext
 def teardown_req(exception=None):
+    """ Closes the session after the request """
     storage.close()
 
 
 if __name__ == "__main__":
-    app.run(debug=True, port='5000', host='0.0.0.0')
+    app.run(port='5000', host='0.0.0.0')
